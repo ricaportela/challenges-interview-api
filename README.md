b502733abcf3cdaab3a1090fd5a9d68d3bc52f71
ricaportela <ricaportela@gmail.com>
2022-07-11 05:57:24 -0300

    Initial Setup

---
+++ README.md
@@ -0,0 +1,13 @@
+Vehicles API with Fastapi
+=========================
+
+Getting Started
+---------------
+
+### Initial Setup ###
+1. Make a new virtualenv: ``python -m venv .venv``
+2. Activate the virtualenv: ``source env/bin/activate``
+3. Install Django: ``pip install -r requirements.txt``
+4. Run the server: ``python main.py``
+6. Open website in browser at ``http://127.0.0.1:8000/vehicles``
+
---
+++ main.py
@@ -0,0 +1,38 @@
+from dataclasses import Field  
+from datetime import datetime
+from typing import List, Union, Optional
+
+from fastapi import FastAPI
+from pydantic.dataclasses import dataclass  # 
+
+
+@dataclass
+class Vehicle:
+    vehicle: str
+    brand: str
+    year: int
+    description: Union[str, None] = None
+    is_sold: Optional[bool] = Field(False, title='Is Sold')    
+    created: datetime
+    updated: datetime
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "vehicle API"}
+
+app.get("/vehicles/", response_model=List[vehicle])  # 
+def get_vehicles():  # 
+    return [  # 
+        {
+            "vehicle": "santana quantum",
+            "brand": "volkswagem",
+            "year": "1996",
+            "description": "bom estado, zerada",
+            "is_sold": "True",
+            "created": "01/01/2022",
+            "updated": "01/01/2022",
+
+        },
+    ]
